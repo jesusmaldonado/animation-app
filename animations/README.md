16e0bce5af50adbdf5bf714189e8874c4ffcc644
jesusmaldonado <jesusmaldonado317@gmail.com>
2015-09-27 17:20:59 -0400

    initial commit

---
+++ animations/README.md
@@ -0,0 +1,13 @@
+# Animation App
+
+Uses NSTimer to continue and stop animation of a gif and update button accordingly
+
+### Tech
+
+* UIImageView
+* NSTimer
+* UIControlState
+
+### GIF
+
+![Image of animation-app](http://i.imgur.com/xecfAzh.gif)
--- animations/ViewController.swift
+++ animations/ViewController.swift
@@ -10,15 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentImageIdx = 1
+    var timer = NSTimer()
+    
+    @IBOutlet var animationButton: UIButton!
+    @IBAction func pauseOrContinueAnimation(sender: AnyObject) {
+        if timer.valid {
+            timer.invalidate()
+            animationButton.setTitle("Continue Animation", forState: UIControlState.Normal)
+        } else {
+            fireTimer()
+            animationButton.setTitle("Stop Animation", forState: UIControlState.Normal)
+        }
+    }
+    
+    func fireTimer() {
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "animate", userInfo: nil, repeats: true)
+    }
+    
+    @IBOutlet var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        fireTimer()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
+    
+    func animate() {
+        if currentImageIdx < 12 {
+            currentImageIdx++
+        } else {
+            currentImageIdx = 1
+        }
+        image.image = UIImage(named: "ys\(currentImageIdx).png")
+    }
+    
+    
+    /*
+    override func viewDidLayoutSubviews() {
+        image.center = CGPointMake(image.center.x - 400, image.center.y)
+        image.alpha = 0
+        image.frame = CGRectMake(100, 20, 0, 0)
+    }
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        UIView.animateWithDuration(1, animations: { () -> Void in
+            self.image.center = CGPointMake(self.image.center.x + 400, self.image.center.y)
+            self.image.alpha = 1
+            self.image.frame = CGRectMake(100, 20, 100, 200)
+        })
+    }
+    */
 
 
 }
